b138a3f95ea5801b26bed12dbd2fbb06df2ac73a
Lana_S <rnasibullina@gmail.com>
2022-09-02 15:33:07 -0400

    Add hello.html

---
+++ Untitled-1.js
@@ -0,0 +1,93 @@
+// Task 1: Build a function-based console log message generator
+function consoleStyler(color, background, fontSize, txt) {
+    var message = "%c" + txt;
+    var style = `color: ${color};`
+    style += `background: ${background};`
+    style += `font-size: ${fontSize};`
+    console.log(message, style);
+}
+
+
+// Task 2: Build another console log message generator
+function celebrateStyler(reason) {
+    var fontStyle = "color: tomato; font-size: 50px";
+    if (reason == "birthday") {
+        console.log(`%cHappy birthday`, fontStyle);
+    } else if (reason == "champions") {
+        console.log(`%cCongrats on the title!`, fontStyle);
+    } else {
+        console.log(message, style);
+    }
+
+}
+
+// Task 3: Run both the consoleStyler and the celebrateStyler functions
+consoleStyler('#1d5c63', '#ede6db', '40px', 'Congrats!');
+celebrateStyler('birthday');
+
+// Task 4: Insert a congratulatory and custom message
+function styleAndCelebrate() {
+consoleStyler(color, background, fontSize, txt);  
+celebrateStyler(reason);
+}
+// Call styleAndCelebrate
+function styleAndCelebrate(){
+    consoleStyler('ef7c8e', 'fae8e0', '30px', 'You made it!');
+    celebrateStyler('champions');
+};
+styleAndCelebrate();
+
+function scopeTest() {
+    var y = 44;
+
+    console.log(x);
+}
+
+var x = 33;
+scopeTest();
+
+class Cake {
+    constructor(lyr) {
+        this.layers = lyr;
+    }
+
+    getLayers() {
+        return this.layers;
+    }
+}
+
+class WeddingCake extends Cake {
+    constructor() {
+        super(2);
+    }
+
+    getLayers() {
+        return super.getLayers() * 5;
+    }
+}
+
+var result = new WeddingCake();
+console.log(result.getLayers());
+class Animal {
+
+}
+
+class Dog extends Animal {
+    constructor() {
+        this.noise = "bark";
+    }
+
+    makeNoise() {
+      return this.noise;
+    }
+}
+
+class Wolf extends Dog {
+    constructor() {
+        super();
+        this.noise = "growl";
+    }
+}
+
+var result = new Wolf();
+console.log(result.makeNoise());
